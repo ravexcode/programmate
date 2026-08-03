3ef80c20c73a5394a39e781d59ddcda8e44178fa
Jose Martinez <rafamartz2007@gmail.com>
2026-08-03 12:20:57 -0600

    0.109.0: Integracion de vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,10 @@ import "./animations.css";
 import "./config.css";
 import "./fonts.css";
 
+//Vercel deploy requirements
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 export const metadata: Metadata = {
   title: "NexZero",
   description: "The aplication from programmers to programmers"
@@ -20,6 +24,8 @@ export default function RootLayout({
     data-scroll-behavior="smooth">
       <link rel="shortcut icon" href="/logos/logo.svg" type="image/x-icon" />
       <body className="bg-black text-zinc-50 font-open-sans">
+        <Analytics />
+        <SpeedInsights />
         {children}
       </body>
     </html>
